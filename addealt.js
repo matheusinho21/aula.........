36449796a841f3e584edd57b8eab9fb1ce2ffe02
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-07-31 09:00:03 -0300

    Create addealt.js

---
+++ addealt.js
@@ -0,0 +1,18 @@
+const pessoa = {
+    nome: "Magali",
+    profissão: "Pedagoga" 
+}
+
+console.log(pessoa.nome) //apresenta os dados
+
+console.log(pessoa.telefone) //Gera uma mensagem reservada
+
+pessoa.telefone = "41 94685-4356" // atribuição de uma propriedade
+
+console.log(pessoa.telefone) // agora vai apresentar o telefone 
+
+console.log(pessoa) //apresenta todo o objeto 
+
+const novaPessoa = {
+    nome: 'teste'
+}
